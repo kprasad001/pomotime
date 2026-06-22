dc06352846351c89239b3ce1964a9f548e107a22
Kishan <prasadkishan2@outlook.com>
2026-06-22 15:40:00 -0600

    Setup the directory

---
+++ pomotime/src/App.tsx
@@ -0,0 +1,15 @@
+	import { useState } from 'react'
+	import './App.css'
+
+	function App() {
+
+		return (
+			<>
+				<section id="center">
+
+				</section>
+			</>
+		)
+		}
+
+	export default App
